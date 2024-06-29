91d1d3db3409947a00cfc01f38c75e0037597af6
ZamDimon <zamdmytro@gmail.com>
2024-06-29 17:46:36 +0300

    :adhesive_bandage: add `base64` response

--- src/api/main.py
+++ src/api/main.py
@@ -4,6 +4,7 @@ import logging
 import hashlib
 import flask
 import ast
+import base64 # For encoding the file
 
 # Internal imports
 import src.api.errors as errs
@@ -59,9 +60,54 @@ def generate_voice() -> None:
         logging.error(f"Failed to process the image: {exception}")
         return jsonify_error(errs.INTERNAL_ERROR)
     
-    # Updating that the claim was submitted
+    # Returning response
     try:
         return flask.send_file(Path('../../' + DEFAULT_PATH), as_attachment=True, mimetype='audio/wav', download_name='lele.wav')
+    except Exception as exception:
+        logging.error(f"Failed while finalizing the request: {exception}")
+        return jsonify_error(errs.INTERNAL_ERROR)
+    
+@app.route("/integrations/voice-cloning/generate-base64", methods=["POST"])
+def generate_voice_base64() -> None:
+    def jsonify_error(err: errs.ErrorResponse) -> Response:
+        json_err, status_code = err
+        return flask.jsonify(json_err), status_code
+    
+    # Asserting that the request has the necessary data
+    def validate_request() -> bool:
+        if "data" not in flask.request.json:
+            return False
+        data = flask.request.json["data"]
+        if "attributes" not in data:
+            return False
+        attributes = data["attributes"]
+        return "text" in attributes
+
+    if not validate_request():
+        logging.error("Bad request!")
+        return jsonify_error(errs.BAD_REQUEST)
+    
+    # Getting attributes
+    attributes = flask.request.json["data"]["attributes"]
+    text = attributes["text"]
+    
+    # Cloning the voice
+    try:
+        voice = voice_cloner.save_voice_locally(text, Path(DEFAULT_PATH))
+    except Exception as exception:
+        logging.error(f"Failed to process the image: {exception}")
+        return jsonify_error(errs.INTERNAL_ERROR)
+    
+    # Returning response
+    try:
+        enc = base64.b64encode(open('../../' + DEFAULT_PATH, 'rb').read())
+        return flask.jsonify({
+            "data": {
+                "attributes": {
+                    "file": enc.decode("utf-8")
+                }
+            }
+        })
     except Exception as exception:
         logging.error(f"Failed while finalizing the request: {exception}")
         return jsonify_error(errs.INTERNAL_ERROR)
